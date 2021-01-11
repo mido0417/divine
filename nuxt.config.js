9409d721c5a1471949ac9ef3c3512d7748b6f45f
Mido <midorin6497@gmail.com>
2021-01-12 03:17:53 +0800

    雛形搬移

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,5 @@
+const webpack = require('webpack')
+
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
@@ -8,12 +10,26 @@ export default {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href: 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css'
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'
+      },
+    ],
+    script: [
+      {
+        src: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js',
+      },
+    ],
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [
+    '@/assets/css/main.scss'
   ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
@@ -33,5 +49,12 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
+    plugins: [
+      new webpack.ProvidePlugin({
+        jQuery: 'jquery',
+        $: 'jquery',
+        'window.jQuery': 'jquery'
+      })
+    ]
   }
 }
---
+++ pages/script.js
@@ -0,0 +1,60 @@
+export default {
+    components: {
+
+    },
+    head: () => ({
+        // title: 'Mido的The-F2E挑戰｜01.TodoList',
+    }),
+    mounted() {
+        $(document).ready(function (e) {
+
+            var xPos = 0;
+            var yPos = 0;
+            var lastScrolled = 0;
+
+
+            var menu_scroll_top1 = $('.menu-wrap nav').offset().top;
+            check_main_menu()
+
+            $(window).scroll(function (e) {
+                check_main_menu();
+
+                var Y = $('#mousefollow').scrollTop();
+                var test = $('#mousefollow').css('top');
+                var num = test
+
+                $('#mousefollow').css({ "left": e.pageX + -10, "top": e.pageY + -10 });
+
+                // console.log(Y)
+                // console.log(+[]=;)
+
+                // console.log($(document).scrollTop())
+
+
+
+            })
+
+            $('#mousefollow').css({ "left": e.pageX + -10, "top": e.pageY + -10 });
+
+            $(document).mousemove(function (e) {
+                $('#mousefollow').css({ "left": e.pageX + -10, "top": e.pageY + -10 });
+            });
+
+            function check_main_menu() {
+                var wt = $(window).scrollTop();
+                var menu_scroll_top2 = $('.menu-wrap nav').offset().top;
+
+                if (wt >= menu_scroll_top2) {
+                    $('.menu-wrap nav').addClass('fix');
+                    // console.log('wt >= menu_scroll_top2')
+                }
+
+                if (wt < menu_scroll_top1) {
+                    $('.menu-wrap nav').removeClass('fix')
+                }
+            }
+
+
+        });
+    },
+};
