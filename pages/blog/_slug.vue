de9c21895463f8c1e6d3104d0678f61c8e58b7fc
Mido <midorin6497@gmail.com>
2021-01-22 08:23:25 +0800

    linkcoin test

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -3,13 +3,18 @@
     <Menu />
     <article class="content-wrap">
       <h1>{{ article.title }}</h1>
-      <p>Article last updated: {{ formatDate(article.updatedAt) }}</p>
+      <p>{{ formatDate(article.updatedAt) }}</p>
       <div
         class="cover-img"
         :style="{ backgroundImage: 'url(' + article.img + ')' }"
       ></div>
       <nuxt-content :document="article" />
-
+      <div
+        class="likecoin-embed likecoin-button"
+        data-liker-id="{mido0417}"
+        :data-href="$nuxt.$route.path"
+      ></div>
+      <script src="https://static.like.co/sdk/v1/button.js"></script>
       <!-- <author :author="article.author" /> -->
 
       <prev-next :prev="prev" :next="next" />
@@ -26,6 +31,63 @@ export default {
   components: {
     Menu,
   },
+
+  head() {
+    return {
+      title: `${this.article.title} | 四月塔羅`,
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: `${this.article.description}`,
+        },
+        {
+          hid: "og:title",
+          property: "og:title",
+          content: `${this.article.title} | 四月塔羅`,
+        },
+        {
+          hid: "og:description",
+          property: "og:description",
+          content: `${this.article.description}`,
+        },
+      ],
+    };
+  },
+
+  // head: {
+  //   title: "四月塔羅 | 探索自我，發現內心的光芒",
+  //   meta: [
+  //     { charset: "utf-8" },
+  //     { name: "viewport", content: "width=device-width, initial-scale=1" },
+  //     {
+  //       hid: "description",
+  //       name: "description",
+  //       content: "探索自我，發現內心的光芒",
+  //     },
+  //     {
+  //       hid: "og:title",
+  //       property: "og:title",
+  //       content: "四月塔羅 | 探索自我，發現內心的光芒",
+  //     },
+  //     {
+  //       hid: "og:description",
+  //       property: "og:description",
+  //       content: "探索自我，發現內心的光芒",
+  //     },
+  //     {
+  //       hid: "og:url",
+  //       property: "og:url",
+  //       content: "https://hellomido.space/divine/",
+  //     },
+  //     {
+  //       hid: "og:image",
+  //       property: "og:image",
+  //       content: "https://images.plurk.com/929Y7dQyKQLkJuWSdI5BS.png",
+  //     },
+  //     { hid: "og:type", property: "og:type", content: "website" },
+  //   ],
+  // },
 
   async asyncData({ $content, params, error }) {
     try {
@@ -63,6 +125,7 @@ export default {
   margin: 0 auto;
   padding: 0 15px;
   max-width: 100%;
+
   @media (min-width: $md) {
     max-width: 1100px;
     padding: 0;
@@ -70,13 +133,18 @@ export default {
 }
 
 .cover-img {
-  max-width: 100%;
-  height: 200px;
-  background-size: cover;
+  background-size: 100%;
   background-position: center;
   background-repeat: no-repeat;
-  margin: 0 -15px;
+  width: 100vw;
+  height: 200px;
+  position: relative;
+  left: 50%;
+  right: 50%;
+  margin-left: -50vw;
+  margin-right: -50vw;
   @media (min-width: $md) {
+    height: 500px;
   }
 }
 </style>
