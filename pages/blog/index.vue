7be6cdde60fa95dbc0d9e7f7d780968be6c2af14
Mido <midorin6497@gmail.com>
2021-01-24 08:08:45 +0800

    setting blog 301

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,25 +1,7 @@
-<template>
-<div id="blog">
-    <Menu />
-  <ArticleList :articles="paginatedArticles" :total="allArticles.length" />  
-  </div>
-  
-</template>
-
 <script>
-import Menu from "@/components/menu/main-menu.vue";
-
 export default {
-  components: {
-    Menu,
-  },
-
-  mounted() {
-    this.$nextTick(() => {
-      this.$nuxt.$loading.start();
-      setTimeout(() => this.$nuxt.$loading.finish(), 500);
-    });
-  },
+  middleware({ redirect }) {
+    return redirect("301", "/blog/page/1");
+  }
 };
 </script>
-
