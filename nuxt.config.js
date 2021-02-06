a9bf6e3db0ef00c0cb7e573cf651d630e8b3ab28
Mido <midorin6497@gmail.com>
2021-02-06 19:30:27 +0800

    install ga

--- nuxt.config.js
+++ nuxt.config.js
@@ -83,6 +83,7 @@ export default {
   modules: [
     '@nuxt/content',
     '@nuxtjs/style-resources',
+    '@nuxtjs/google-gtag',
 
   ],
   styleResources: {
@@ -90,6 +91,31 @@ export default {
       './assets/scss/_style.scss',
     ]
   },
+
+  'google-gtag': {
+    id: 'G-EWL6N33DVQ', // required
+    config: {
+      // this are the config options for `gtag
+      // check out official docs: https://developers.google.com/analytics/devguides/collection/gtagjs/
+      anonymize_ip: true, // anonymize IP 
+      send_page_view: false, // might be necessary to avoid duplicated page track on page reload
+      // linker: {
+      //   domains: ['domain.com', 'domain.org']
+      // }
+    },
+    debug: true, // enable to track in dev mode
+    disableAutoPageTrack: false, // disable if you don't want to track each page route with router.afterEach(...)
+    // optional you can add more configuration like [AdWords](https://developers.google.com/adwords-remarketing-tag/#configuring_the_global_site_tag_for_multiple_accounts)
+    // additionalAccounts: [{
+    //   id: 'AW-XXXX-XX', // required if you are adding additional IDs
+    //   config: {
+    //     send_page_view: false // optional configurations
+    //   }
+    // }]
+  },
+
+
+
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
     plugins: [
