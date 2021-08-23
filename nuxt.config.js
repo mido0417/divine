42a8dfdb6a17a58326f00f0dccd1ad46fbbaec8e
Mido <midorin6497@gmail.com>
2021-08-23 15:58:17 +0800

    config設定

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,15 +15,15 @@ export default {
     htmlAttrs: {
       lang: 'zh-TW'
     },
-    title: '生命之歌 | 探索自我，發現內心的光芒',
+    title: 'Claire | 探索自我，發現內心的光芒',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '探索自我，發現內心的光芒' },
       { hid: 'og:locale', property: 'og:locale', content: 'zh-TW' },
-      { hid: 'og:title', property: 'og:title', content: '生命之歌 | 探索自我，發現內心的光芒' },
+      { hid: 'og:title', property: 'og:title', content: 'Claire | 探索自我，發現內心的光芒' },
       { hid: 'og:description', property: 'og:description', content: '探索自我，發現內心的光芒' },
-      { hid: 'og:url', property: 'og:url', content: 'http://www.life-song.space' },
+      { hid: 'og:url', property: 'og:url', content: 'https://clairelin.space/' },
       { hid: 'og:image', property: 'og:image', content: 'https://images.unsplash.com/photo-1494797262163-102fae527c62?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=700&q=80' },
       { hid: 'og:type', property: 'og:type', content: 'website' },
     ],
@@ -47,7 +47,7 @@ export default {
       // },
       {
         rel: "canonical",
-        href: 'http://www.life-song.space',
+        href: 'https://clairelin.space/',
       }
     ],
     script: [
@@ -128,7 +128,7 @@ export default {
   },
 
   sitemap: {
-    hostname: 'https://www.life-song.space/',
+    hostname: 'https://clairelin.space/',
     exclude: [
       '/script',
       '/blog'
